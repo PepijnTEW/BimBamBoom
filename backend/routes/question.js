447c9bed2eb4f6f3638222a04ab4499b47fcec95
PepijnTEW <pepijntewinkel@gmail.com>
2026-03-11 22:40:25 +0100

    fix(routes): add () after router before the router function wasn't called

--- backend/routes/question.js
+++ backend/routes/question.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router = express.Router;
+const router = express.Router();
 const Question = require('../models/question');
 
 router.get('/questions', async(req, res) => {
